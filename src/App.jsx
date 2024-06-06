1510ae3ac78586c4e775f59c4b513d347197a56d
Yann Duhamel <yann.duhamel@gmail.com>
2024-06-06 13:52:54 +0200

    initialization commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <h1>
+        mixology mate
+        <br /> est en construction
+      </h1>
+      <div className="subcontainer">
+        <a href="https://www.linkedin.com/in/yannduhamel/" target="_blank">linkedin</a>
+        <a href="https://portfolio-yann-duhamel.vercel.app/" target="_blank">portfolio</a>
+      </div>
+    </div>
+  );
+}
+
+export default App;
